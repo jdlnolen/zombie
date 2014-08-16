3d075240ffeacb70925a3f3e17d2d777b07598f9
vagrant <vagrant@precise64.(none)>
2014-08-16 16:18:19 +0000

    added mocha and removed makefile

--- configure.sh
+++ configure.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#configuring the system
-#wget https://raw.githubusercontent.com/jdlnolen/zombie/master/Makefile
-
 #install meteor
 curl https://install.meteor.com | /bin/sh
 
 #install spiderable package
 meteor add spiderable
+
+#installing mocha
+npm install -g mocha
 
 #installing meteorite
 npm install -g meteorite
